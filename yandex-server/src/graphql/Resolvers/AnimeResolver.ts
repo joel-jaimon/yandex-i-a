756ec9520cf88db5b36e576f90a1bd91ed4bd79f
Joel Jaimon <joel.jaimon99@gmail.com>
2021-06-08 23:12:38 +0530

    anime search resolver added

--- yandex-server/src/graphql/Resolvers/AnimeResolver.ts
+++ yandex-server/src/graphql/Resolvers/AnimeResolver.ts
@@ -1,4 +1,5 @@
 import { Arg, Args, Mutation, Query, Resolver } from "type-graphql";
+import { ILike } from "typeorm";
 import { Animes } from "../../entity/anime";
 import { CreateAnimeArgs } from "../ArgsType/AnimeArgs";
 
@@ -10,6 +11,55 @@ export class AnimeResolver {
       where: {
         id: id,
       },
+    }).catch((e) => {
+      throw new Error(e);
+    });
+
+    return response;
+  }
+
+  @Query(() => [Animes])
+  async animeList(
+    @Arg("skip") skip: number,
+    @Arg("take") take: number
+  ): Promise<Animes[]> {
+    const response = await Animes.find({
+      skip: skip,
+      take: take,
+      order: {
+        id: "ASC",
+      },
+    }).catch((e) => {
+      throw new Error(e);
+    });
+
+    return response;
+  }
+
+  @Query(() => [Animes])
+  async searchAnime(
+    @Arg("search_query") search_query: string,
+    @Arg("skip") skip: number,
+    @Arg("take") take: number
+  ): Promise<Animes[]> {
+    const response = await Animes.find({
+      where: [
+        {
+          genres: ILike(`%${search_query} #%`),
+        },
+        {
+          title: ILike(`%${search_query} #%`),
+        },
+        {
+          type: ILike(`%${search_query} #%`),
+        },
+      ],
+      skip: skip,
+      take: take,
+      order: {
+        id: "ASC",
+      },
+      cache: true,
     }).catch((e) => {
       throw new Error(e);
     });
